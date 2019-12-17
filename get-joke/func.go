056ec6d28529cc96158b74c2c998aef656670063
Maarten Mulders <m.th.mulders@xs4all.nl>
2019-12-17 14:12:03 +0100

    Populate joke collection and return a random one

--- get-joke/func.go
+++ get-joke/func.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"func/jokes"
 	"io"
 
 	fdk "github.com/fnproject/fdk-go"
@@ -13,17 +14,26 @@ func main() {
 	fdk.Handle(fdk.HandlerFunc(myHandler))
 }
 
-type Person struct {
+type person struct {
 	Name string `json:"name"`
 }
 
+func readPersonFromRequest(in io.Reader) person {
+	person := &person{Name: "World"}
+	json.NewDecoder(in).Decode(person)
+	return *person
+}
+
 func myHandler(ctx context.Context, in io.Reader, out io.Writer) {
-	p := &Person{Name: "World"}
-	json.NewDecoder(in).Decode(p)
+	person := readPersonFromRequest(in)
+
 	msg := struct {
-		Msg string `json:"message"`
+		Greeting string `json:"greeting"`
+		Joke     string `json:"joke"`
 	}{
-		Msg: fmt.Sprintf("Hello %s", p.Name),
+		Greeting: fmt.Sprintf("Hello %s", person.Name),
+		Joke:     jokes.GetRandomJoke(),
 	}
+
 	json.NewEncoder(out).Encode(&msg)
 }
---
+++ get-joke/jokes/jokes.go
@@ -0,0 +1,32 @@
+package jokes
+
+import (
+	"math/rand"
+	"time"
+)
+
+var jokes [8]string
+var seed int64
+
+func init() {
+	if len(jokes[0]) != 0 {
+		return
+	}
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	jokes[0] = "My Xmas sweater had a lot of static electricity. The store gave me a new one, free of charge."
+	jokes[1] = "There was a man that couldn't see that well, so he fell into it"
+	jokes[2] = "How does Bono spell \"Color?\" With or without u?"
+	jokes[3] = "A man sued an airline after it lost his luggage. Sadly, he lost his case."
+	jokes[4] = "What's the fastest growing city on earth? The capital of Ireland, it's Dublin every day"
+	jokes[5] = "My friend didn't understand cloning. That makes two of us"
+	jokes[6] = "If you put your left shoe on the wrong foot, it's on the right foot"
+	jokes[7] = "I tried to organize a Hide and Seek tournament, but I eventually gave up. Good players are hard to find."
+}
+
+// GetRandomJoke returns a pseudo-random joke from the collection.
+func GetRandomJoke() string {
+	idx := rand.Intn(len(jokes))
+	return jokes[idx]
+}
