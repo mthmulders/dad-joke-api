220371a9643ca5415d9d891e484534d61269ce80
Maarten Mulders <maartenm@infosupport.com>
2019-12-25 21:00:43 +0100

    Replace env.sh with terraform.tfvars

--- infra/env.example.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export TF_VAR_compartment_ocid=
-export TF_VAR_user_ocid=
-export TF_VAR_tenancy_ocid=
-export TF_VAR_fingerprint=
-export TF_VAR_private_key_path=
-export TF_VAR_private_key_password=
-export TF_VAR_region=
