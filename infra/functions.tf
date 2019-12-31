f8cbea6958cf38e1d03b5c711fc03b41f0419835
Maarten Mulders <maartenm@infosupport.com>
2019-12-31 08:11:33 +0100

    Bump version number

--- infra/functions.tf
+++ infra/functions.tf
@@ -9,7 +9,7 @@ resource "oci_functions_application" "dad-jokes" {
 resource "oci_functions_function" "get-joke" {
   application_id = oci_functions_application.dad-jokes.id
   display_name   = "get-joke"
-  image          = "fra.ocir.io/frwqejk9in9h/dad-jokes/get-joke:0.0.5"
+  image          = "fra.ocir.io/frwqejk9in9h/dad-jokes/get-joke:0.0.7"
   memory_in_mbs  = "128"
 }
 
