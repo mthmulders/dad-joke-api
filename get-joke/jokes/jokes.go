557df8d0817f9e02527c41d4d85775a868311c1d
Maarten Mulders <m.th.mulders@xs4all.nl>
2020-01-24 11:35:09 +0100

    Punctuation

--- get-joke/jokes/jokes.go
+++ get-joke/jokes/jokes.go
@@ -16,12 +16,12 @@ func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	jokes[0] = "My sweater had a lot of static electricity. The store gave me a new one, free of charge."
-	jokes[1] = "There was a man that couldn't see that well, so he fell into it"
+	jokes[1] = "There was a man that couldn't see that well, so he fell into it."
 	jokes[2] = "How does Bono spell \"Color\"? With or without u?"
 	jokes[3] = "A man sued an airline after it lost his luggage. Sadly, he lost his case."
-	jokes[4] = "What's the fastest growing city on earth? The capital of Ireland, it's Dublin every day"
-	jokes[5] = "My friend didn't understand cloning. That makes two of us"
-	jokes[6] = "If you put your left shoe on the wrong foot, it's on the right foot"
+	jokes[4] = "What's the fastest growing city on earth? The capital of Ireland, it's Dublin every day."
+	jokes[5] = "My friend didn't understand cloning. That makes two of us."
+	jokes[6] = "If you put your left shoe on the wrong foot, it's on the right foot."
 	jokes[7] = "I tried to organize a Hide and Seek tournament, but I eventually gave up. Good players are hard to find."
 	jokes[8] = "Why do programmers prefer dark mode? Because light attracts bugs."
 	jokes[9] = "Why did the hipster burn his mouth? He drank the coffee before it was cool."
